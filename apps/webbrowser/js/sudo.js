18068623cbf974048a8440262d843e57ab72cfc2
AST <153839847+angelotrabuco2013@users.noreply.github.com>
2024-02-10 19:09:22 -0300

    Update sudo.js

--- apps/webbrowser/js/sudo.js
+++ apps/webbrowser/js/sudo.js
@@ -69,9 +69,8 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 	}
 	fetchProxy (url, options, i) {
 		const proxies = (options || {}).proxies || [
-			'https://yacdn.org/proxy/',
-			'https://api.codetabs.com/v1/proxy/?quest='
-		]
+                "https://api.codetabs.com/v1/proxy/?quest="
+                ]
 		return fetch(proxies[i] + url, options).then(res => {
 			if (!res.ok)
 				throw new Error(`${res.status} ${res.statusText}`);
